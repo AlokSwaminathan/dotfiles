dc0980f98db2563ecc664bd7350274f1734b43a3
Alok Swaminathan <swaminathanalok@gmail.com>
2024-09-15 11:37:31 -0400

    Update status bar so it shows tailscale status

--- .config/sway/connect-tailscale.sh
+++ .config/sway/connect-tailscale.sh
@@ -6,4 +6,6 @@ server_ip="100.104.52.34"
 # make sure you add this command to sudoers so sway can run it without needing a password
 sudo tailscale set --exit-node=$server_ip --exit-node-allow-lan-access=true
 
+pkill -SIGRTMIN+4 i3status-rs
+
 notify-send -t 5000 -a "Sway" "Tailscale" "Tailscale connected to exit node $server_name $output"
--- .config/sway/disconnect-tailscale.sh
+++ .config/sway/disconnect-tailscale.sh
@@ -2,4 +2,6 @@
 
 sudo tailscale set --exit-node= --exit-node-allow-lan-access=true
 
+pkill -SIGRTMIN+4 i3status-rs
+
 notify-send -t 5000 -a "Sway" "Tailscale" "Tailscale exit node disconnected"
