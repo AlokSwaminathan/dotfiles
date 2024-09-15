6a76ce5cafc4be4f7a36d1a8e298f69aca3bad2e
Alok Swaminathan <swaminathanalok@gmail.com>
2024-09-15 19:40:57 -0400

    Pnpm stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,3 +35,10 @@ if status is-login
         exec sway -d 2>sway.log
     end
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/aswaminathan/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
