e8fbb4a66a59fafa23c77800b218f3c89460c52d
Alok Swaminathan <swaminathanalok@gmail.com>
2024-11-28 22:40:38 -0500

    chore: enable correct formatters

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -19,7 +19,7 @@ M = {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {}
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -31,6 +31,8 @@ M = {
         python = { "isort", "black" },
         javascript = { "prettier" },
         typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        javascriptreact = { "prettier" },
         c = { "clang-format" },
         cpp = { "clang-format" },
         -- Conform can also run multiple formatters sequentially
